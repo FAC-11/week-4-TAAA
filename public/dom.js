8e57fe58ff0cb8e9dfbe64051e8532d0d476c618
Morkeltry <morkeltry@mailfence.com>
2017-07-20 16:53:23 +0100

    working dropdown populate and select functions. Aint pretty tho ;)

---
+++ public/dom.js
@@ -0,0 +1,34 @@
+
+var testing = ['ahab', 'Aha the band from the 80s', 'Abraham Lincoln', 'bourbons'];
+
+
+function clickable (ev) {
+  var inputBox = document.getElementById('myInput');
+  inputBox.value = ev.target.textContent;
+};
+
+
+function appendAList (resultsArray) {
+  var prevList = document.getElementById ('results-list');
+  if (prevList) {
+    prevList.parentNode.removeChild(prevList);
+  }
+  var newList = document.createElement ('ul');
+  if (resultsArray.length) {
+    newList.addClass = 'liStyle'
+    resultsArray.forEach (function(el) {
+      var newButton = document.createElement ('button');
+      newButton.textContent = el;
+      // newButton.addClass ('add-functionality');
+      newButton.addEventListener ('click', clickable);
+
+      newList.appendChild(newButton);
+
+    });
+  }
+
+  document.getElementById ('dropdown-section').appendChild(newList);
+}
+
+
+// appendAList(testing);
