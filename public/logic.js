5a3289fbabd922b2fdc02e00ffb2084292951438
Morkeltry <morkeltry@mailfence.com>
2017-07-20 17:05:25 +0100

    inculded cb function on request event listener

--- public/logic.js
+++ public/logic.js
@@ -1,10 +1,12 @@
 var logic = {
-  sendTextToApi: function() {
+  sendTextToApi: function(cbDealWithResults) {
     var txt = document.getElementById('myInput').value;
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
-
+        //    (assuming that the stringified object has an array member called resultsArray)
+        var resultsArray   //  var resultsArray = JSONparse(xhr.responseText.resultsArray);
+        cbDealWithResults (resultsArray);
       }
     };
     xhr.open("GET", '/API?' + txt, true);
