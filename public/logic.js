e7c9e681d5ec07bf79f1075eed77fcdff6134908
Morkeltry <morkeltry@mailfence.com>
2017-07-20 17:27:00 +0100

    pointed to resultsArray as sent from serverside

--- public/logic.js
+++ public/logic.js
@@ -5,7 +5,7 @@ var logic = {
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
         //    (assuming that the stringified object has an array member called resultsArray)
-        var resultsArray   //  var resultsArray = JSONparse(xhr.responseText.resultsArray);
+        var resultsArray = JSONparse(xhr.responseText.resultsArray);
         cbDealWithResults (resultsArray);
       }
     };
