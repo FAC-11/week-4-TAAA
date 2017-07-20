49ed53138bd790106dbb5dbb91a479709e69faa2
AseelM <aseelmustafa@outlook.com>
2017-07-20 16:00:53 +0100

    created shrink array function
    #5

--- src/backend.js
+++ src/backend.js
@@ -1,8 +1,16 @@
 function compareFunction (string, arrayOfStrings){
-  return arrayOfStrings.filter(function(el){
+  var unshrunkResults = arrayOfStrings.filter(function(el){
     var regex = new RegExp('^'+string+'\\w*');
+    shrinkFunction(unshrunkResults);
     return el.match(regex);
+
   })
+}
+
+function shrinkFunction(arrayOfStrings){
+
+return arrayOfStrings.length <=  5 ? arrayOfStrings : arrayOfStrings.slice(0,5);
+
 }
 
 module.exports = {
