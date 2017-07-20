d79c0e3b6ffa6caf1dea04350620016943a971c1
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-07-20 13:01:31 +0100

    api function is calling backend logic :ok_hand:

--- src/api.js
+++ src/api.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const path = require('path');
+const backend = require('./backend.js');
+const dinoData = require('./dinosaurs.json');
 
 function apiRequest(endpoint, response) {
   var queryString = endpoint.split("").slice(5).join("");
   console.log(queryString);
+  // console.log(dinoData.dinosaurs);
+
+console.log(backend.compareFunction(queryString, dinoData.dinosaurs));
+
   console.log('received API request. \nsee HTML.');
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.end('received API request. \nfiling request in bin.\njob done.', endpoint);
