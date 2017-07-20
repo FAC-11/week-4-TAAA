8879b71d8b5cd6472abb4e8f7f0d5b3b55a89cb4
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-07-20 15:43:06 +0100

    Remove console log from logic.js and api.js Relates #2

--- public/logic.js
+++ public/logic.js
@@ -1,19 +1,13 @@
 var logic = {
   sendTextToApi: function() {
     var txt = document.getElementById('myInput').value;
-
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log('ready');
+
       }
     };
-
     xhr.open("GET", '/API?' + txt, true);
     xhr.send();
-    console.log(xhr);
-
-    // test purposes
-    // document.getElementById('demo').textContent = txt;
   }
 }
