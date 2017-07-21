95625ebb3a0d7d7059d376656b9d2674d7e295a0
astroash <astroaisha@gmail.com>
2017-07-21 11:36:26 +0100

    removed loop in handlers had to move const to after first if statement, to fix html doc relates #50

--- src/handlers.js
+++ src/handlers.js
@@ -10,15 +10,6 @@ const handler = (request, response) => {
   console.log(endpoint + ', ' + method);
   // console.log (typeof(endpoint));
 
-  const contentType = {
-    // '/favicon.ico': 'image/x-icon',
-    '/index.html': 'text/html',
-    '/main.css': 'text/css',
-    '/dom.js': 'application/javascript',
-    '/logic.js': 'application/javascript',
-    '/background.jpg': 'image/jpeg',
-  };
-
   if (endpoint.startsWith('/API')) {
     apiRequest(endpoint, response);
   }         /////this block ('/API' is what we need to fill in. separate out querystring.)
@@ -29,8 +20,17 @@ const handler = (request, response) => {
       console.log('Got blank');
     }
 
-    if (endpoint in contentType) {
-      response.writeHead(200, { 'Content-Type': contentType[endpoint] });
+    const contentType = {
+      // '/favicon.ico': 'image/x-icon',
+      '/index.html': 'text/html',
+      '/main.css': 'text/css',
+      '/dom.js': 'application/javascript',
+      '/logic.js': 'application/javascript',
+      '/background.jpg': 'image/jpeg',
+    }[endpoint];
+
+    if (contentType) {
+      response.writeHead(200, { 'Content-Type': contentType});
       fs.readFile(path.join(__dirname, '..', 'public', endpoint), (error, file) => {
         if (error) {
           console.log(error);
@@ -38,12 +38,12 @@ const handler = (request, response) => {
           response.end("There was a file error at our end. Sorry about that. Go have a cup of tea or something. Maybe we'll have fixed it...");
           return;
         }
-        response.writeHead(200, { 'Content-Type': contentType[endpoint] });
+        response.writeHead(200, { 'Content-Type': contentType});
         response.end(file);
       });
     }
     else {
-      console.log ('404:',endpoint, contentType[endpoint]);
+      console.log ('404:',endpoint, contentType);
       response.writeHead(404, { 'Content-Type': 'text/html'});
       response.end('Nuttin');
     }
