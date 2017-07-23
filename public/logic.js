b4296653b874483cfb68e3045711469c7849a48f
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-07-23 11:15:12 +0100

    Add client side textfield value checking Relates #41

--- public/logic.js
+++ public/logic.js
@@ -3,6 +3,11 @@
   function sendTextToApi(callback) {
 
     var txt = document.getElementById('myInput').value;
+    // if string is empty or contains non-alphabetical characters
+    if (txt === '' || txt.match(/[^A-Z]/ig)) {
+      return callback([]);
+    }
+
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
